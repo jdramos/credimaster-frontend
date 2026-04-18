802f349e7b4f7df2212c6c0ea19313465dda3ae1
Jose Daniel Ramos <jdramos@hotmail.es>
2026-04-18 11:38:11 -0600

    Fix API env variable name

--- src/api.js
+++ src/api.js
@@ -1,20 +1,16 @@
 // src/api.js
-import axios from 'axios';
-
+import axios from "axios";
 
 const token = process.env.REACT_APP_API_TOKEN;
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000'; // Default to localhost if not set
-
+const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const API = axios.create({
-    baseURL: baseURL, // Adjust if needed
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': token, // Uncomment if using token-based auth
-        // Add other headers if needed
-    },
+  baseURL: baseURL, // Adjust if needed
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: token, // Uncomment if using token-based auth
+    // Add other headers if needed
+  },
 });
-
-;
 
 export default API;
